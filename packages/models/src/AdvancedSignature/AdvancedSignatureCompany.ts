ce3b4f0bd39c7b422d307df9bd3fa897692a1e38
Ulises Rafael Ramirez Martinez <ulisesraf.ramirez@gmail.com>
2023-06-30 12:59:54 -0600

    add models for signature

---
+++ packages/models/src/AdvancedSignature/AdvancedSignatureCompany.ts
@@ -0,0 +1,14 @@
+export interface AdvancedSignatureCompany {
+  id: string;
+
+  name: string;
+
+  privateKey: string;
+  publicKey: string;
+
+  totalTemplates: number;
+  totalDocs: number;
+
+  createdAt: string;
+  updatedAt: string;
+}
