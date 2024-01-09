cf80ebea10d1ff855acf9707fb8f5173e217ec02
Ulises Rafael Ramirez Martinez <ulisesraf.ramirez@gmail.com>
2024-01-09 12:18:08 -0600

    restore cjs support

--- packages/api/rollup.config.js
+++ packages/api/rollup.config.js
@@ -10,6 +10,11 @@ module.exports = {
       format: 'esm',
       sourcemap: true,
     },
+    {
+      file: 'dist/cjs/index.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
   ],
   plugins: [
     typescript({
