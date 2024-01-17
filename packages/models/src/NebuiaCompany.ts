053fd9b3d81dc13b57f7b8a2fd1f5ef27d940d5b
Ulises Rafael Ramirez Martinez <ulisesraf.ramirez@gmail.com>
2024-01-17 08:33:46 -0600

    Upgrade model for nebuia company

--- packages/models/src/NebuiaCompany.ts
+++ packages/models/src/NebuiaCompany.ts
@@ -1,10 +1,5 @@
 import { NebuiaStepNames } from './NebuiaSteps';
 
-export type NebuiaCompanyWidgetSettings = {
-  dark_mode?: boolean;
-  primary_color?: string;
-  secondary_color?: string;
-};
 export type NebuiaRawKeys =
   | {
       public_key?: string;
@@ -15,14 +10,33 @@ export type NebuiaRawKeys =
       secret_key: string;
     };
 
+export type NebuiaCompanyCreditSettings = {
+  domain?: string;
+  platform_name?: string;
+  collaborator_document_config?: {
+    document_name: string;
+    document_type: string;
+  }[];
+};
+
+export type NebuiaCompanySettings = {
+  image?: string;
+  hook?: string;
+  primary_color?: string;
+  secondary_color?: string;
+  credit: NebuiaCompanyCreditSettings;
+};
+
 export type NebuiaCompany = {
   id: string;
   name: string;
-  ip: string;
-  origin: string;
+  ip?: string;
+  origin?: string;
+  origins?: string[];
   steps: NebuiaStepNames[];
   keys?: NebuiaRawKeys;
-  settings?: { widget?: NebuiaCompanyWidgetSettings };
+  settings: NebuiaCompanySettings;
+  credits?: number;
   otp?: { secret: string };
   admin?: string;
 };
