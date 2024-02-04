80cf93032edade3a21f8b159689587a48c3f43a0
Ulises Rafael Ramirez Martinez <ulisesraf.ramirez@gmail.com>
2024-02-04 16:16:29 -0600

    Pass base url

--- packages/sdk/src/credits/NebuiaCreditsEnrollment.ts
+++ packages/sdk/src/credits/NebuiaCreditsEnrollment.ts
@@ -2,8 +2,8 @@ import { NebuiaCreditsEnrollmentRepository } from '@nebuia-ts/api';
 import { NebuiaKeys } from '@nebuia-ts/models';
 
 export class NebuiaCreditsEnrollment extends NebuiaCreditsEnrollmentRepository {
-  constructor(keys: NebuiaKeys) {
-    super();
+  constructor(keys: NebuiaKeys, baseUrl?: string) {
+    super(baseUrl);
     this.keys = keys;
   }
 }
